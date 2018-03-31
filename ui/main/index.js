464e2b73e61f09965c7f9b181e945b8a4b14484d
jebbs <qjebbs@gmail.com>
2018-03-31 12:31:01 +0800

    add templates and tutorial

--- ui/main/index.js
+++ ui/main/index.js
@@ -1,11 +1,17 @@
+var templates = {
+    "googleTranslate": `// gtranslate translates the match text, eg. English to Simplified Chinese
+// change the "en" and "zh-cn" to whatever source and target language you want
+// don't forget to configurate "superReplace.googleApiKey"
+gtranslate("en","zh-cn")`,
+    "uppercase": "s=>s.toUpperCase()"
+}
+
 window.addEventListener("load", () => {
     sendMsg = document.getElementById("sendMsg");
     textFind = document.getElementById("textFind");
     textReplace = document.getElementById("textReplace");
     textFunc = document.getElementById("textFunc");
     radRngSelection = document.getElementById("radRngSelection");
-
-
     btnDoReplace = document.getElementById("doReplace");
     btnDoReplace.addEventListener("click", doReplace);
 });
@@ -22,4 +28,18 @@ function doReplace() {
         // console.log(sendMsg.attributes["href"].value);
         sendMsg.click();
     }
+}
+
+function onListClick(e) {
+    let tmpl = "";
+    let name = "";
+    let attr = e.attributes["data"]
+    if (!attr || !attr.value) {
+        tmpl = "// ERROR: Template list item data soruce not set.";
+    } else {
+        name = attr.value;
+        tmpl = templates[name];
+        if (!tmpl) tmpl = `// ERROR: Cannot find template '${name}'`;
+    }
+    if (tmpl) textFunc.value = tmpl;
 }
